82b7792d2af2736783e84681fbc73bdf6c080881
ehacker98 <36097426+ehacker98@users.noreply.github.com>
2018-02-03 22:54:59 -0500

    Fixed dropdown errors; fixed inline error chrome reporting

---
+++ js/dropdownhandler.js
@@ -0,0 +1,22 @@
+document.getElementById("onclick-test").addEventListener("click", myEvent);
+
+function myEvent(){
+	 document.getElementById("myDropdown").classList.toggle("show");
+}
+
+document.getElementById("myInput").addEventListener("keyup", filterEvent);
+
+function filterEvent() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+        if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+        } else {
+            a[i].style.display = "none";
+        }
+    }
+}
